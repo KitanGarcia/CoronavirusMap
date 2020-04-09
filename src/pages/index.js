2ed76d16d21cdbef0eda73ea56ef09e869d3ca93
Kitan Garcia <kitangar@gmail.com>
2020-04-08 23:09:31 -0700

    isolate geoJSON information from api data

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,10 +40,88 @@ const IndexPage = () => {
     //destructure response.
     //Equivalent to const data = response.data;
     const {data = []} = response;
-
     //data is now a list of countries as below:
     //[{"country":"USA","countryInfo":{"_id":840,"iso2":"US","iso3":"USA","lat":38,"long":-97,"flag":"https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/us.png"},"updated":1586326293551,"cases":400540,"todayCases":205,"deaths":12857,"todayDeaths":16,"recovered":21711,"active":365972,"critical":9169,"casesPerOneMillion":1210,"deathsPerOneMillion":39,"tests":2082431,"testsPerOneMillion":6291}, ...]
     console.log(data);
+
+    //boolean if data is an array and is not empty
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    /***GeoJSON Format***/
+    /*
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [125.6, 10.1]
+        },
+        "properties": {
+          "name": "Dinagat Islands"
+        }
+      }
+    */
+    /***GeoJSON Format***/
+    
+    //loop through features
+    //For each country, obtain lat and lng to create a point on the map
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        //countryInfo = country.info
+        const {countryInfo = {} } = country;
+        //lat = countryInfo.lat; lng = countryInfo.long;
+        const {lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+          //... spread syntax means the iterable (array in this case) is expanded to its individual elements
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+          }
+        }
+      })
+    }
+    console.log("GEOJSON");
+    //By expanding the output below and inserting into geojson.io, we get a marker that appears
+    console.log(geoJson);
+    /*LOOKS LIKE THIS IN GEOJSON.IO
+{
+  "type": "Feature",
+  "geometry": {
+    "coordinates": [-97, 38],
+    "type": "Point"
+  },
+  "properties": {
+    "active": 397442,
+    "cases": 435128,
+    "casesPerOneMillion": 1315,
+    "country": "USA",
+    "countryInfo": {
+      "flag": "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/us.png",
+      "iso2": "US",
+      "iso3": "USA",
+      "lat": 38,
+      "long": -97,
+      "_id": 840
+    },
+    "critical": 9279,
+    "deaths": 14795,
+    "deathsPerOneMillion": 45,
+    "recovered": 22891,
+    "tests": 2209041,
+    "testsPerOneMillion": 6674,
+    "todayCases": 201,
+    "todayDeaths": 7,
+    "updated": 1586409093924
+  }
+}
+    */
+
   }
 
   const mapSettings = {
